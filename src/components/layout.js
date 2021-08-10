1dda8efc63178f3abdb004ef71a432842f5a63a4
Yulianto <yuli.anto@icube-mobile.com>
2021-08-10 15:45:49 +0700

    Add Navbar & Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,64 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
+import "../styles";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
+const TemplateWrapper = ({ footerData = null, navbarData = null, children }) => (
+  <div>
+    <Helmet>
+      <html lang="en" />
+      <meta name="keywords" content="montreal, javascript, programming, meetup" />
+    </Helmet>
+    <Navbar data={navbarData} />
+    <main>{children}</main>
+    <Footer data={footerData} />
+  </div>
+);
+
+export const query = graphql`
+  fragment LayoutFragment on Query {
+    footerData: allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "footer" } } }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            logoImage {
+              image
+              imageAlt
+              tagline
+            }
+            socialLinks {
+              image
+              imageAlt
+              label
+              linkURL
+            }
+          }
+        }
+      }
+    }
+    navbarData: allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "navbar" } } }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            logoImage {
+              image
+              imageAlt
+            }
+            menuItems {
+              label
+              linkType
+              linkURL
+            }
+          }
+        }
+      }
+    }
   }
+`;
 
-  return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </div>
-  )
-}
-
-export default Layout
+export default TemplateWrapper;
